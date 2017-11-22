78a3b92efe695654c627d90f14eaec4caad834d2
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-22 11:55:59 -0500

    adds landing-page component

--- src/components/container.js
+++ src/components/container.js
@@ -4,11 +4,15 @@ import { Route, Switch } from 'react-router-dom';
 import './container.css';
 
 import EventsList from './events-list';
+import LandingPage from './landing-page';
 
 export default function Container(props) {
   return (
     <main role="main" className="main">
-      <EventsList name="All your events so far" />
+      <Switch>
+        <Route exact path="/" component={ LandingPage } />
+        <Route exact path="/events-list" component={ EventsList } />
+      </Switch>
     </main>
   );
 }
--- src/components/events-list.js
+++ src/components/events-list.js
@@ -7,7 +7,7 @@ export default function EventsList(props) {
   return(
     <section>
       <header role="banner">
-        <h1>{props.name}</h1>
+        <h1>All your events so far</h1>
       </header>
       <EventSummary eventType='Thanksgiving' date='Nov 23 2016' name='Night in Philly' guestCount='10' food='1 12lbs turkey' drinks='5 bottles of wine' />
       <EventSummary eventType='New years' date='Jan 1 2017' name='A very merry xmas' guestCount='10' food='2lbs Lentils, Ham' drinks='5 bottles of wine, 5 dom perrignon' />
