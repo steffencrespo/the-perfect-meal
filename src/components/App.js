d96de35cae5cf61d7862f66213fcde94899693f3
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-22 17:12:02 -0500

    renames components in order to try to fix failed import

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './app.css';
+import './App.css';
 
 import Navbar from './navbar';
 import Container from './container';
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
-import App from './components/app.js';
+import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
