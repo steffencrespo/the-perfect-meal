e09ac33488ab34ab5911ba746f7e7adfd8c9ee30
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-30 16:18:49 -0500

    adds flex display property to EventSummary component

--- src/components/event-summary.js
+++ src/components/event-summary.js
@@ -5,11 +5,11 @@ import './event-summary.css';
 export default function EventSummary(props) {
   return (
     <section key={ props.index }>
-      <header>
+      <header className="flex_summary_header">
         <h2>{ props.eventType } - { props.date }</h2>
         <p>{ props.name }</p>
       </header>
-      <p>{ props.guestCount }</p>
+      <p> Guests: { props.guestCount }</p>
       <p>For this event you needed { props.drinks } drinks and { props.food } of food</p>
       <button>Clone</button>
       <button>Delete</button>
