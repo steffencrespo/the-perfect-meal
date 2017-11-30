9f394716449df2624a9342aa18da8d0e54854e59
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-29 22:43:11 -0500

    renders page using the components available in the redux state

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -7,24 +7,26 @@ import './events-list.css';
 
 export class EventsList extends Component  {
 
-  listEvents() {
-    this.props.events.map( (event) => {
-      
-    });
-  }
-
   render() {  
-      return(
-        <section>
-          <header role="banner">
-            <h1>All your events so far</h1>
-          </header>
-          <EventSummary eventType='Thanksgiving' date='Nov 23 2016' name='Night in Philly' guestCount='10' food='1 12lbs turkey' drinks='5 bottles of wine' />
-          <EventSummary eventType='New years' date='Jan 1 2017' name='A very merry xmas' guestCount='10' food='2lbs Lentils, Ham' drinks='5 bottles of wine, 5 dom perrignon' />
-          <EventSummary eventType='Easter' date='Jan 1 2017' name='A very merry xmas' guestCount='10' food='2lbs Lentils, Ham' drinks='5 bottles of wine, 5 dom perrignon' />
-        </section>
-      );
-    }
+    const allEvents = this.props.events.map((event) => 
+      <EventSummary 
+        eventType={event.eventType} 
+        date='Nov 23 2017'
+        name={event.partyName} 
+        guestCount={event.partyGuests} 
+        food='1 12lbs turkey' 
+        drinks='2 bottles of wine' />
+    );
+
+    return(
+      <section>
+        <header role="banner">
+          <h1>All your events so far</h1>
+        </header>
+        {allEvents}
+      </section>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
