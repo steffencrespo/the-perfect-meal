fbf4ba840ecf565c783c59036905404f40147afd
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-29 22:13:15 -0500

    flips navbar items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,8 +7,8 @@ export default function Navbar(props) {
   return (
     <nav className="navbar">
       <ul>
-        <li><Link to="/events-list">Past Events </Link></li>
         <li><Link to="/new-event">New Event </Link></li>
+        <li><Link to="/events-list">Past Events </Link></li>
         <li style={{ float: 'right' }}><a className="active" href="#about">Logout</a></li>
       </ul>
     </nav>
