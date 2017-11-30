4bd65a8973bcd12b5924a7740bb68aa3f29a7a14
Leonardo Steffen <leosteffen84@gmail.com>
2017-11-30 15:09:51 -0500

    adds font awesome; modifies nav to make it easier to use flexbox

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,12 +5,10 @@ import './navbar.css';
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar">
-      <ul>
-        <li><Link to="/new-event">New Event </Link></li>
-        <li><Link to="/events-list">Past Events </Link></li>
-        <li style={{ float: 'right' }}><a className="active" href="#about">Logout</a></li>
-      </ul>
-    </nav>
+    <ul className="nav">
+      <li><Link to="/new-event"><i className="fa fa-spoon"></i>New Event </Link></li>
+      <li><Link to="/events-list"><i className="fa fa-history"></i>Past Events </Link></li>
+      <li style={{ float: 'right' }}><a className="active" href="#about">Logout</a></li>
+    </ul>
   );
 }
